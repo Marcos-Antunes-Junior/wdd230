ee478487131f0bcfdeebcde20acacbdbd721bf41
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-01-28 15:25:39 -0300

    'Created the Barra Lighthouse Chamber home page.'

---
+++ chamber/scripts/script.js
@@ -0,0 +1,27 @@
+const year = document.querySelector("#year");
+let lastModified = document.querySelector("#lastModified");
+
+//Display on the screen the date of the last time there was a modification.
+lastModified.textContent = document.lastModified;
+
+//Display on the screen the current year.
+year.textContent = `${new Date().getFullYear()}`;
+
+const datefield = document.querySelector(".date");
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+
+
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
+
+// What does toggle mean? Instead of add and remove, toggle means add the class name (the parameter, which in this case is named 'responsive') if it does not currently exist, and remove the 
