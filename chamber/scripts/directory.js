3c1e43880b45a325bb9acb6f29ce60435bd2b1d2
Marcos-Antunes-Junior <mantunes140397@gmail.com>
2023-03-01 18:36:46 -0300

    'Added lesson09 and directory pages.'

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,68 @@
+const url = "./data.json"
+
+async function getCompaniesData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.companies);
+    displayCompanies(data.companies);
+  }
+  
+  getCompaniesData(url);
+
+
+
+  const displayCompanies = (companies) => {
+    const cards = document.querySelector('main div'); 
+  
+    companies.forEach((company) => {
+    
+        let card = document.createElement('div');
+        card.setAttribute("class", "directory-section");
+        let name = document.createElement('h2');
+        let portrait = document.createElement('img');
+        let phone = document.createElement('h4');
+        let address = document.createElement('h4');
+        let email = document.createElement('h4');
+ 
+
+
+     
+     name.textContent = company.name;
+     phone.textContent = company.phone;
+     address.textContent = company.address;
+     email.textContent = company.email;
+
+
+      portrait.setAttribute('src', company.imageurl);
+      portrait.setAttribute('alt', `Portait of ${company.name}`);
+      portrait.setAttribute('loading', 'lazy');
+  
+
+      card.appendChild(name);
+      card.appendChild(portrait);
+      card.appendChild(phone);
+      card.appendChild(address);
+      card.appendChild(email);
+      cards.appendChild(card);
+
+      const gridbutton = document.querySelector("#grid");
+      
+      
+      const listbutton = document.querySelector("#list");
+
+      gridbutton.addEventListener("click", () => {
+        // example using arrow function
+        cards.classList.add("cards");
+        cards.classList.remove("list");
+    });
+
+    listbutton.addEventListener("click", () => {
+        // example using arrow function
+        cards.classList.add("list");
+        cards.classList.remove("cards");
+
+    });
+
+
+    }) 
+  } 
---
+++ lesson09/scripts/prophets.js
@@ -0,0 +1,92 @@
+
+
+const year = document.querySelector('#year');
+let lastModified = document.querySelector('#lastModified');
+
+lastModified.textContent = document.lastModified;
+
+year.textContent = `${new Date().getFullYear()}`;
+
+const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+
+
+async function getProphetData(url) {
+    const response = await fetch(url);
+    const data = await response.json();
+    //console.table(data.prophets);
+    displayProphets(data.prophets);
+  }
+  
+  getProphetData(url);
+
+
+const displayProphets = (prophets) => {
+    const cards = document.querySelector('div.cards'); 
+  
+    prophets.forEach((prophet) => {
+    
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let portrait = document.createElement('img');
+      let birth = document.createElement('h4');
+      let place = document.createElement('h4');
+      let children = document.createElement('h4');
+      let div = document.createElement('div');
+      let years = document.createElement('h4');
+      let death = document.createElement('h4');
+      let convertBirthDate = Date.parse(prophet.birthdate);
+      let convertDeathDate = Date.parse(prophet.death);
+      let convertedAge = convertDeathDate - convertBirthDate;
+      let checkAge = Math.floor(convertedAge / 31536000000);
+      let age = document.createElement('h4');
+
+
+  
+     
+      if(prophet.order == '1'){
+        h2.textContent = `${prophet.name} ${prophet.lastname} ${prophet.order}st Latter-day President`;
+      }else if(prophet.order == '2'){
+        h2.textContent = `${prophet.name} ${prophet.lastname} ${prophet.order}nd Latter-day President`;
+      }else if(prophet.order == '3'){
+        h2.textContent = `${prophet.name} ${prophet.lastname} ${prophet.order}rd Latter-day President`;
+      }else{
+        h2.textContent = `${prophet.name} ${prophet.lastname} ${prophet.order}th Latter-day President`;
+      }
+     
+      birth.textContent = `Birth: ${prophet.birthdate}`;
+      place.textContent = `Place: ${prophet.birthplace}`;
+      children.textContent = `Children: ${prophet.numofchildren}`;
+      years.textContent = `Prophet Years: ${prophet.length}`;
+      
+      if(prophet.death != null){
+      death.textContent = `Death: ${prophet.death}`;
+      }
+
+      if(prophet.death != null){
+        age.textContent = `Age: ${checkAge}`;
+      }else{
+        age.textContent = `Age: 98`
+      }
+      
+      portrait.setAttribute('src', prophet.imageurl);
+      portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+      portrait.setAttribute('loading', 'lazy');
+      portrait.setAttribute('width', '340');
+      portrait.setAttribute('height', '440');
+  
+      card.appendChild(h2);
+      div.appendChild(birth);
+      div.appendChild(place);
+      div.appendChild(children);
+      div.appendChild(years);
+      div.appendChild(death);
+      div.appendChild(age);
+      card.appendChild(div);
+      card.appendChild(portrait);
+      cards.appendChild(card);
+    }) 
+  } 
+
+
+
+  
